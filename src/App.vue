f3dab760862d481748112bca9e65d87c68b1f071
SalvLav <richardjthies95@gmail.com>
2022-11-30 11:25:36 -0600

    updated components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"> <!--template tag contain only 1 child element, aka the div. Put everything in the div if you need things in the template.-->
+    <h1>Would you rather...?</h1>
+
+    <would-you-rather v-bind:question="wyrQuestion"
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-changed="answerChanged"> <!--connects the data to the props. AnswerChange calls method to receive event/message from child component-->
+      
+    </would-you-rather> <!--Must reference other components in the template in order to use their elements. Could also use WouldYouRather instead, and the reference would still work.-->
+    <label>{{userSelectionMessage}}</label>
+
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data() { // data is being sent to WouldYouRather component, as it is the child component
+    return {
+      wyrQuestion : "Would you rather dance or sing?",
+      wyrAnswer1: "Dance",
+      wyrAnswer2: "Sing",
+      userSelectionMessage: "" // empty string as no choice has been made yet
+    }
+  },
+  methods: {
+    answerChanged(choice) { //recieves the data from the event sent by the child component
+      this.userSelectionMessage = `Thanks! You have chosen to ${choice.toLowerCase()}.` //updates userSelectionMessage
+    }
   }
 }
 </script>
 
 <style>
+
+body {
+  background: lightslategray;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +51,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background: lightgoldenrodyellow
 }
 </style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wyr">
+    <h2>Please make your choice.</h2>
+
+    <h3>{{question}}</h3>
+
+    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade"> <!--needs v-bind because a radio buttob doesn't have a value by default, it starts empty. v-bind sets the values-->
+    <label>{{answer1}}</label>
+
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade"> <!--v-on change calls a method to send a message to parent component-->
+    <label>{{answer2}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: { // data received from parent components is stored in prop values. "data()" is generated/managed by components for internal use
+    question: String, // defining props works by putting a type (String), rather than a value
+    answer1: String, // Child component (which has the props) does not modify "question", answer1", or "answer2". Create new data function instead.
+    answer2: String
+  },
+  data() { // data() is what WouldYouRather.vue is generating itself, rather than receivng from parent component. It figures out what the radio buttion input choice is
+    return { // returns object
+      choice: ''
+    }
+  },
+  methods: {
+    choiceMade() {
+      this.$emit('answer-changed', this.choice) // data can be sent with event to the parent
+      // ^^$emit is a Vue method to send a message/event to parent component. Name can be anything (don't use  built-in JS names such as click/change) name with ''-'' will always be valid
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+.wyr {
+  border: 3px red solid;
+  background: bisque;
+  margin: 20px;
+}
+
+</style>
